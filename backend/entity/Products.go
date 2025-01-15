a2f4c40e2cd31abfdc93b95661303b5b279ea362
Admin <sorrawit456flookza@gmail.com>
2025-01-15 11:34:44 +0700

    ทำ Unit Test การบันทึกข้อมูล SKU ไม่สำเร็จ - close #6

--- backend/entity/Products.go
+++ backend/entity/Products.go
@@ -8,5 +8,5 @@ type Products struct{
 	gorm.Model
 	Name string `valid:"required~Name is required"`
 	Price float64 `valid:"required~Price is required,range(1|1000)~Price must be between 1 and 1000"`
-	SKU string `valid:"required~SKU is required,matches(^[A-Z][A-Z][A-Z]\\d{5})~SKU must start with 3 uppercase English letters (A-Z) followed by \n\t5 digits (0-9)\""`
+	SKU string `valid:"required~SKU is required,matches(^[A-Z][A-Z][A-Z]\\d{5})~SKU must start with 3 uppercase English letters (A-Z) followed by 5 digits (0-9)"`
 }
